a5e47b61ba214578d5fee5bfcec2537cbea37c4f
Tyler Niswonger <tylerniswonger@gmail.com>
2020-03-08 17:41:46 -0500

    Nested form functionality

--- app/models/test_order.rb
+++ app/models/test_order.rb
@@ -1,3 +1,5 @@
 class TestOrder < ApplicationRecord
     has_many :test_products
+
+    accepts_nested_attributes_for :test_products
 end
--- app/models/test_product.rb
+++ app/models/test_product.rb
@@ -2,4 +2,12 @@ class TestProduct < ApplicationRecord
   # Added by Tyler 2/11, for testing products
   belongs_to :test_order
   has_many_attached :images
+
+  rails_admin do
+    nested do
+      configure :test_order do
+        hide
+      end
+    end
+  end
 end
