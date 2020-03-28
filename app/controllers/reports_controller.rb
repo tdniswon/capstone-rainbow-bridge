550210c5fdb64fde6ecc159abe0f5a83f0b20c83
Daniel P <kingalpine@gmail.com>
2020-03-28 11:28:55 -0700

    Added chartkick and Daniel's reports (WIP)

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,11 +1,13 @@
 class ReportsController < ApplicationController
     def index
-        @report = "test_report"
+        @report = "test_report2"
     end
+
 
     def new
         puts params[:report]
         @report = params[:report]
         render "reports/index"
+
     end
 end
