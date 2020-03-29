7ac034e8d7b66586dddffa7d8b43312653469c4b
Daniel P <kingalpine@gmail.com>
2020-03-28 21:51:50 -0700

    regex fix

--- app/models/rental_item.rb
+++ app/models/rental_item.rb
@@ -2,7 +2,7 @@ class RentalItem < ApplicationRecord
     has_many :rental_lines
 
     validates :rental_item_name, presence: true
-    validates :rental_item_cost,  allow_blank: true, format: {with: /\A\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(\.[0-9][0-9])?+\z/, message: 'Format: $XXX.XX'}
+    validates :rental_item_cost,  allow_blank: true, format: {with: /\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(\.[0-9][0-9])?+/, message: 'Format: $XXX.XX'}
     rails_admin do
         object_label_method :display_name
     end
