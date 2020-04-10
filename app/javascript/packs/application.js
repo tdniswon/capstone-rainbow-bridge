24d5e3620809c09ff3c1c85d5b1f242ab7a21b8f
tdniswon <42559467+tdniswon@users.noreply.github.com>
2020-04-10 11:57:32 -0500

    updates for chartkick charts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
-require("bootstrap")
-require("chartkick")
-require("chart.js")
-require("Chart.bundle")
+// require("jquery")
+// require("bootstrap")
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -22,6 +20,8 @@ require("Chart.bundle")
 import 'bootstrap'
 import './src/application.scss'
 import 'cocoon'
+require("chartkick")
+require("chart.js")
 
 document.addEventListener("turbolinks:load", () => {
     $('[data-toggle="tooltip"]').tooltip()
